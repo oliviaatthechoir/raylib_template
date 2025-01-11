18c6aa697f253e010c9d72bd5c3c79e72a82036b
oliviaatthechoir <124530511+oliviaatthechoir@users.noreply.github.com>
2025-01-11 22:07:05 +0100

    Revert "deleted unused code from main"
    This reverts commit 4743956da25230680f6e47e20cb8f863131c620c.

--- Source/main.cpp
+++ Source/main.cpp
@@ -59,9 +59,18 @@ int main(void)
     {
         // Update
         //----------------------------------------------------------------------------------
-    
+        // TODO: Update your variables here
         //----------------------------------------------------------------------------------
 
+        //if (IsKeyPressed(KEY_SPACE))
+        //{
+        //    PlaySound(sound);
+        //}
+
+        //if (IsKeyPressed(KEY_BACKSPACE))
+        //{
+        //    StopSound(sound);
+        //}
 
         game.Update();
       
