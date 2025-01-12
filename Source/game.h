21c8f0ca7786b88a22bb5dd77048b7b466226af4
oliviaatthechoir <124530511+oliviaatthechoir@users.noreply.github.com>
2025-01-12 23:04:39 +0100

    trying to get rid of this char array

--- Source/game.h
+++ Source/game.h
@@ -197,7 +197,7 @@ public:
 
 
 	//TEXTBOX ENTER
-	char name[9 + 1] = "\0";     //One extra space required for null terminator char '\0'
+	std::string name = "";     //One extra space required for null terminator char '\0'
 	int letterCount = 0;
 
 	Rectangle textBox = { 600, 500, 225, 50 };
