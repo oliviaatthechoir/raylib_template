fca2074295ffd7d2ab20e5cc3154815fc81846c8
oliviaatthechoir <124530511+oliviaatthechoir@users.noreply.github.com>
2025-01-16 17:15:17 +0100

    Update Resources.h
    looks like presentation

--- Source/Resources.h
+++ Source/Resources.h
@@ -7,22 +7,37 @@
 #include <string>
 #include <array>
 #include "raylib.h"
-#include "vector"
+#include <vector>
+#include <string_view>
 
 struct GameTexture {
-	Texture2D texture; 
+	Texture2D texture = {}; 
 	explicit GameTexture(std::string_view path) {
 		texture = LoadTexture(path.data()); 
 	}
 
+GameTexture(const GameTexture&) = delete; 
+GameTexture& operator=(const GameTexture&) = delete;
+
+ GameTexture(GameTexture&& other) noexcept : texture(other.texture) {
+        other.texture = {}; // Leave the moved-from object in a valid state
+    }
+    GameTexture& operator=(GameTexture&& other) noexcept {
+        if (this != &other) {
+            UnloadTexture(texture); // Clean up current texture
+            texture = other.texture;
+            other.texture = {}; // Leave the moved-from object in a valid state
+        }
+        return *this;
+    }
+
 
 	~GameTexture() {
-		UnloadTexture(texture); 
-	}
-
+		 if (texture.id != 0) {
+            UnloadTexture(texture);
+        }
+		
 	
-
-
 	
 };
 
@@ -33,6 +48,19 @@ struct Resources
 	GameTexture alienTexture = GameTexture("./Assets/Alien.png");
 	GameTexture barrierTexture = GameTexture("./Assets/Barrier.png");
 	GameTexture laserTexture = GameTexture("./Assets/Laser.png");
+
+	Resources()
+        : shipTextures{
+            GameTexture("./Assets/Ship1.png"),
+            GameTexture("./Assets/Ship2.png"),
+            GameTexture("./Assets/Ship3.png")
+          },
+          alienTexture("./Assets/Alien.png"),
+          barrierTexture("./Assets/Barrier.png"),
+          laserTexture("./Assets/Laser.png") {}
+
+    // Destructor: Automatically cleans up all resources
+    ~Resources() = default;
 };
 
 
