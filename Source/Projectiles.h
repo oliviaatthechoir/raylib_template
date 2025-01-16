8f1a79faa2a251f5a4db1694bef9a2adaaf88f50
oliviaatthechoir <124530511+oliviaatthechoir@users.noreply.github.com>
2025-01-16 17:51:02 +0100

    Update Projectiles.h
    projectiles up to date with presentation presentation

--- Source/Projectiles.h
+++ Source/Projectiles.h
@@ -14,10 +14,19 @@ public:
 	bool active = true;
 	EntityType type; 
 
+	Projectile() = default; 
+
+
+	 Projectile(Vector2 initialPosition = {0, 0}, int initialSpeed = 15, EntityType entityType = EntityType::Projectile)
+    : position(initialPosition), speed(initialSpeed), active(true), type(entityType) {}
+
+
 	std::vector<Projectile> projectile;
 	
 
 	void Update();
 
 	void Render(const GameTexture& texture) const;
+
+	~Projectile() = default; 
 };
